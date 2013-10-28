19643bef3af81cec88c32de8da64eb95c0216612
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-10-28 18:09:46 +0100

    Time.zone.local

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,14 +16,15 @@ class Event
 
   validates_presence_of :date_start, :time_start, :date_end, :time_end
 
-  DATES_FIX = [:start, :end]
   before_save :set_dates
 
   def set_dates
-    DATES_FIX.each do |fix|
-      year, month, day = self.send("date_#{fix}").split('-').map(&:to_i)
-      hour, minutes = self.send("time_#{fix}").split(':').map(&:to_i)
-      self.send("datetime_#{fix}=", DateTime.new(year, month, day, hour, minutes))
-    end
+    year, month, day = self.date_start.split('-').map(&:to_i)
+    hour, minutes = self.time_start.split(':').map(&:to_i)
+    self.datetime_start = Time.zone.local(year, month, day, hour, minutes)
+
+    year, month, day = self.date_end.split('-').map(&:to_i)
+    hour, minutes = self.time_end.split(':').map(&:to_i)
+    self.datetime_end = Time.zone.local(year, month, day, hour, minutes)
   end
 end
