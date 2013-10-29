c28a92781cd7d37a6f6d69542606a5e26b1ffbae
Patrycja Radaczynska <trycjapa@gmail.com>
2013-10-29 17:52:49 +0100

    testing tests and css changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,4 @@
-class EventsController < ApplicationController
+  class EventsController < ApplicationController
   before_action :set_event
 
   def index
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe EventsController do
-
+  it "should create event" do
+    event = Event.new(:name => "Foo", :description => "Bar")
+  end
+  it "should redirect to root_path" do
+    redirect_to root_path
+  end
 end
